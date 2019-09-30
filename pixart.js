e36a8f9a2c97104653d9ff6907b92a41d03e6146
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-09-30 16:48:31 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+let brush = document.getElementsByClassName('brush')[0]
+let setColor = document.getElementById('set-color')
+
+setColor.addEventListener('click', (e) => {
+    e.preventDefault()
+    let color = document.getElementById('color-field').value
+    brush.style.backgroundColor = color
+})
+
 
