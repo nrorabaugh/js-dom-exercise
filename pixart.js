7508d85e0361bc93a1dd4485b4f6ecaaec1c8a8a
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-09-30 17:22:41 -0400

    Commit 3

--- pixart.js
+++ pixart.js
@@ -1,15 +1,22 @@
 let brush = document.getElementsByClassName('brush')[0]
 let setColor = document.getElementById('set-color')
+let color = ''
 
 setColor.addEventListener('click', (e) => {
     e.preventDefault()
-    let color = document.getElementById('color-field').value
+    color = document.getElementById('color-field').value
     brush.style.backgroundColor = color
 })
 
-for (let i = 0; i <= 20; i ++) {
+for (let i = 0; i < 20; i++) {
     let square = document.createElement('div')
     square.className = 'square'
     document.getElementsByTagName('body')[0].appendChild(square)
 }
 
+let squarez = document.getElementsByClassName('square')
+for (let i = 0; i < squarez.length; i++) {
+    squarez[i].addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = color
+    })
+}
